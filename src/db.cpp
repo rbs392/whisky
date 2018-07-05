e5df160b749fe9e0796015f8d732f7502960e9da
rbs392 <rbs392@yahoo.com>
2018-07-05 16:00:31 +0530

    db open complete

--- src/db.cpp
+++ src/db.cpp
@@ -1,6 +1,9 @@
 #include "db.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <sys/uio.h>
 
 #define IDXLEN_SZ   4;
 #define SEP         ':';
@@ -21,14 +24,14 @@ typedef unsigned long COUNT;
 
 typedef struct {
     int     idxfd;
-    int     datafd;
+    int     datfd;
     char    *idxbuf;
-    char    *databuf;
+    char    *datbuf;
     char    *name;
     off_t   idxoff;
     size_t  idxlen;
-    off_t   dataoff;
-    size_t  datalen;
+    off_t   datoff;
+    size_t  datlen;
 
     off_t   ptrval;
     off_t   ptroff;
@@ -79,6 +82,57 @@ db_open(const char *pathname, int oflag, ...)
     if((db == _db_alloc(len)) == NULL)
         printf("db_open: _db_alloc error for DB");
 
+    db->nhash   = NHASH_DEF;
+    db->hashoff = HASH_OFF;
+
+    strcpy(db->name, pathname);
+    strcat(db->name, ".idx");
+
+    if (oflag & O_CREAT) {
+        // uses <stdarg.h>
+        va_list ap;
+        va_start(ap, oflag);
+        mode = va_arg(ap, int);
+        va_end(ap);
+        db->idxfd = open(db->name, oflag, mode);
+        strcpy(db->name + len, ".dat");
+        db->datfd = open(db->name, oflag, mode);
+    } else {
+        db->idxfd = open(db->name, oflag);
+        strcpy(db->name + len, ".dat");
+        db->datfd = open(db->name, oflag);
+    }
+
+    if(db->idxfd < 0 || db->datfd < 0) {
+        _db_free(db);
+        return (NULL);
+    }
+
+    if( (oflag & (O_CREAT | O_TRUNC)) == (O_CREAT | O_TRUNC)) {
+        if(writew_lock(db->idxfd, 0, SEEK_SET, 0) < 0)
+            printf("db_open: writew_lockerror");
+
+        if(fstat(db->idxfd, &statbuff) < 0)
+            printf("db_open: fstat error");
+
+        if(statbuff.st_size == 0) {
+            sprintf(asciiptr, "%*d", PTR_SZ, 0);
+            hash[0] = 0;
+
+            for(i=0; i< NHASH_DEF+1; i++)
+                strcat(hash, asciiptr);
+
+            strcat(hash, "\n");
+            i = strlen(hash);
+
+            if(write(db->idxfd, hash, i) != i)
+                printf("db_open: index file init write error");
+        }
+        if(un_lock(db->idxfd, 0, SEEK_SET, 0) < 0)
+            printf("db_open: un_lock error");
+    }
+    db_rewind(db);
+    return(db);
 }
 
 
